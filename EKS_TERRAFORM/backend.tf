76e9494ec82395a64227ee4f57f5f6a9d11e4129
Prasad852 <95183694+Prasad852@users.noreply.github.com>
2024-04-08 19:46:41 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "durga-uber-90321" # Replace with your actual S3 bucket name
+    bucket = "durga-uber-903211" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
